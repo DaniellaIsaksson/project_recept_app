6d0e12b5ba51724ace77c98ef595d92398419618
DaniellaIsaksson <daniellas.isaksson@gmail.com>
2023-09-27 12:51:44 +0200

    Added Navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styling/App.css";
 import { Home } from "./pages/home";
 import Header from "./components/header";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <Home />
     </>
---
+++ src/components/navbar.js
@@ -0,0 +1,35 @@
+import "../styling/App.css";
+
+function Navbar() {
+  return (
+    <ul class="nav nav-underline justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">
+          Home
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Test
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Test
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Test
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Test
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navbar;
